899d1e9d461c0ed34a37cf359c87804514079be9
HrantH-yan <hrant.harutyunyan6795@gmail.com>
2023-01-21 00:56:47 +0400

    fixed 1st loop condition

--- Stack_Prefix.cpp
+++ Stack_Prefix.cpp
@@ -1,42 +1,39 @@
 #include <iostream>
 #include <stack>
 
+
 int main()
 {
     int ans = 0;
     char sym;
     std::stack<char> Elements; 
     std::stack<char> Prefix;   
-    std::cout<<"Enter an expression which consists of digits, '+' and '*' operators \n";
+    std::cout<<"Enter an expression: Only digits, '+' and '*' operators \n";
     std::cout<<"Add '=' at the end:\n";
 
     int check = 0;
-    while (true)  // checking if the input is correct and filling Elements stack with expression
-    {
-        std::cin>>sym;
-        if (sym == '=')
-        {
-            break;
-        }
-        if ((!isdigit(sym) && (check == 0)) || ((sym != '*' && sym != '+') && (check == 1)))  
-        // checking if the input is correct and filling Elements stack with expression
+    std::cin>>sym;
+    while (sym != '=') // checking input and filling 'Elements' with expression
+    {  
+        if ((!isdigit(sym) && (check == 0)) || (sym != '*' && sym != '+' && (check == 1)))  
         {                                                                              
-            std::cout<<"Incorrect input: it shoud be like 1+2*6*9+4...= ";
-            return 0;
+            std::cerr<<"Incorrect input: correct example '1+2*6*9+4...+1 ='\n";
+            return 1;
         }
         check = (check ^ 1);
         Elements.push(sym);
+        std::cin>>sym;
     }
     if (check == 0)
     {
         Elements.pop();
     }
 
-    while (!Elements.empty())        // Filling Prefix stack with prefix expression (from right to left) ignoring '+'-es
-    {                                // becouse if it's not '*' then it's '+'
-        if(isdigit(Elements.top()))  // (for example 1+2*3+4*5*6 (+ + 1 * 2 3 * 4 * 5 6) --> 6 5 * 4 * 3 2 * 1)
-        {                                  
-            Prefix.push(Elements.top());
+    while (!Elements.empty())              // Filling Prefix stack with prefix expression
+    {                                      // (from right to left) ignoring '+'-es
+        if(isdigit(Elements.top()))        // becouse if it's not '*' then it's '+'
+        {                                  // 1+2*3+4*5*6 (++1*2 3*4*5 6)-->(6 5*4*3 2*1)
+            Prefix.push(Elements.top());    
             Elements.pop();
         }
         else if(Elements.top() == '+')
@@ -51,7 +48,7 @@ int main()
             Elements.pop();
         }
     }
-   
+    
     int temp;    // for multiplication
     while(!Prefix.empty())
     {
@@ -74,6 +71,6 @@ int main()
         }
     }
     
-    std::cout<<"equals to "<<ans<<'\n';
+    std::cout<<"Equal to "<<ans<<'\n';
     return 0;
 }
